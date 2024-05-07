c1e47446d91d04c53e24a1373d323256d85a3343
Anandi Karunaratne <anandi.karunaratne@gmail.com>
2024-05-08 09:17:31 +1000

    Unit Tests for Insertion

--- src/main/java/org/anandi/nip/nip/insertion/InsertionNoiseInjector.java
+++ src/main/java/org/anandi/nip/nip/insertion/InsertionNoiseInjector.java
@@ -8,45 +8,46 @@ import java.util.Set;
 
 public abstract class InsertionNoiseInjector implements NoiseInjector {
 
-    InsertionNoiseInjector(Set<String> activities){ }
+    Set<String> activities;
 
-    abstract Trace injectActivity(Trace cleanTrace, int insertIndex);
+    InsertionNoiseInjector(Set<String> activities){
+        this.activities = activities;
+    }
+
+    abstract void injectActivity(Trace cleanTrace, int insertIndex);
 
     /**
      * can we have mixed situations too?
      * some noise consecutively, and other at random places?
      */
     @Override
-    public Trace injectNoise(Trace cleanTrace, int length) {
+    public void injectNoise(Trace cleanTrace, int length) {
         double methodDecider = Math.random();
         double probability = 0.5;
         if (methodDecider < probability) {
-            cleanTrace = insertConsecutiveActivities(cleanTrace, length);
+            insertConsecutiveActivities(cleanTrace, length);
         } else {
-            cleanTrace = insertRandomActivities(cleanTrace, length);
+            insertRandomActivities(cleanTrace, length);
         }
-        return cleanTrace;
     }
 
     /**
      * test this
      */
-    Trace insertConsecutiveActivities(Trace cleanTrace, int length) {
+    void insertConsecutiveActivities(Trace cleanTrace, int length) {
         Random random = new Random();
-        int startIndex = random.nextInt(cleanTrace.size() - length); // recheck edge cases
+        int startIndex = random.nextInt(cleanTrace.size() - length + 1); // recheck edge cases
         for (int i = 0; i < length; i++) {
-            cleanTrace = injectActivity(cleanTrace, i + startIndex);
+            injectActivity(cleanTrace, i + startIndex);
         }
-        return cleanTrace;
     }
-    Trace insertRandomActivities(Trace cleanTrace, int length) {
+    void insertRandomActivities(Trace cleanTrace, int length) {
         Random random = new Random();
         int insertIndex;
         for (int i = 0; i < length; i++) {
-            insertIndex = random.nextInt(cleanTrace.size());
-            cleanTrace = injectActivity(cleanTrace, insertIndex);
+            insertIndex = random.nextInt(cleanTrace.size() + 1);
+            injectActivity(cleanTrace, insertIndex);
         }
-        return cleanTrace;
     }
 
 }
